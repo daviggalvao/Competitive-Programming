09656838b6a600254498c14760336de25a3213c8
Davi Galvão <daviguerra27@gmail.com>
2025-09-08 16:38:26 -0300

    modify 2 sets

--- CSES/1092.cpp
+++ CSES/1092.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
+
 int main(void){
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     long long n, tmp;
@@ -10,12 +10,10 @@ int main(void){
     if(sum%2==0){
         cout << "YES\n";
         sum/=2;
-        set<long long> sum1, sum2;
+        set<long long> sum1;
         while(1){
             sum -= tmp;
-            if(sum>0){
-                sum1.insert(tmp);
-            }
+            if(sum>0) sum1.insert(tmp);
             if(sum<=0){
                 sum+=tmp;
                 sum1.insert(sum);
@@ -23,18 +21,15 @@ int main(void){
             }
             tmp--;
         }
+        cout << n-sum1.size() << '\n';
         for(long long i=1; i<n; i++){
             auto it = sum1.find(i);
             if(it == sum1.end()){
-                sum2.insert(i);
+                cout << i << ' ';
             }
         }
-        cout << sum1.size() << '\n';
+        cout << '\n' << sum1.size() << '\n';
         for(auto i=sum1.begin(); i!=sum1.end(); i++){
-            cout << *i << ' ';
-        }
-        cout << '\n' << sum2.size() << '\n';
-        for(auto i=sum2.begin(); i!=sum2.end(); i++){
             cout << *i << ' ';
         }
     }
